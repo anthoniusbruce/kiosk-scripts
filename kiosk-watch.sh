ad2cd46f17d7b66fa0887d8848bf84eb1f06d2c0
Andy Collins <anthonius.bruce@gmail.com>
2016-12-08 21:33:11 +0000

    Fixing the inotifywait so that it does not freak out at normal use but still allows for restart

--- kiosk-watch.sh
+++ kiosk-watch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #while 1; do
-  while inotifywait -e close_write -e close_nowrite /var/www/html/kiosk/sites; do 
+  while inotifywait -e close_write -e attrib /var/www/html/kiosk/sites; do 
     /usr/kiosk-scripts/kiosk-restart.sh;
   done
 #done
