79a8885907e7c736b022ec5188ddb0ac97fd4b6f
Andy Collins <anthonius.bruce@gmail.com>
2017-03-02 16:02:29 +0000

    Adding script that will post the IP address to a google form

---
+++ notify_ipaddress.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ip=$(hostname --all-ip-addresses)
+
+curl 'https://docs.google.com/a/onvio.com/forms/d/e/1FAIpQLSdwcGUhTPYmIVM5BTNUQjbtw9PMLC2gIqxcMaK1_fD-i_16Pw/formResponse' --data 'entry.319187029='"$ip" --compressed
