328dae4fa730996f3cb41c7303563727cc4dd62c
anthoniusbruce <anthonius.bruce@gmail.com>
2024-12-06 10:33:02 -0500

    removed outdated ip address nonsense

--- notify_ipaddress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 60
-
-ip=$(hostname --all-ip-addresses)
-
-curl 'https://docs.google.com/a/onvio.com/forms/d/e/1FAIpQLSdwcGUhTPYmIVM5BTNUQjbtw9PMLC2gIqxcMaK1_fD-i_16Pw/formResponse' --data 'entry.319187029='"$ip" --compressed
